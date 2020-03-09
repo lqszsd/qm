1df710f9e33d0d8dfa55bf29a77700b7907855fa
lq <1658616397@qq.com>
2020-03-09 10:50:00 +0800

    1

--- pages/info/index.vue
+++ pages/info/index.vue
@@ -67,36 +67,6 @@
 				playState:0,//播放状态
 				audioCont:'',
 				audioList:[
-					{name:'你的微笑',
-					 author:'Shart Foeir',
-					 src:'http://cdnringbd.shoujiduoduo.com/ringres/userv1/a48/556/66405556.aac',
-					 img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547630730529&di=f37e409de12f9930e25507c3252a462c&imgtype=0&src=http%3A%2F%2Fmp2.iqiyipic.com%2Fimage%2F20180827%2F03%2F41%2Fh_1199242471_h_601_400_400.jpg',
-					 },
-					{name:'英雄联盟群雄齐聚',
-					 author:'毛不易',
-					 src:'http://cdnringbd.shoujiduoduo.com/ringres/userv1/a48/414/66335414.aac',
-					 img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547630762297&di=2c3317bf38f107287414b1b0cd33f0e5&imgtype=0&src=http%3A%2F%2Fsingerimg.kugou.com%2Fuploadpic%2Fpass%2Fsofthead%2F400%2F20140829%2F20140829161553861786.jpg',
-					 },
-					{name:'TryEverything',
-					 author:'Shakira',
-					 src:'http://cdnringbd.shoujiduoduo.com/ringres/userv1/a48/480/66379480.aac',
-					 img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547471202346&di=bd9b5e27beb9341b058d21e521e8d75e&imgtype=0&src=http%3A%2F%2Fb-ssl.duitang.com%2Fuploads%2Fitem%2F201707%2F27%2F20170727210705_zcijE.jpeg',
-					 },
-					{name:'我爱过几个人',
-					 author:'邓论',
-					 src:'http://cdnringbd.shoujiduoduo.com/ringres/userv1/a48/339/65390339.aac',
-					 img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547630610810&di=1a0c6d9557aa2bba822c54694a55429e&imgtype=0&src=http%3A%2F%2Fmp0.iqiyipic.com%2Fimage%2F20180827%2Fff%2F79%2Fh_1199249539_h_601_400_400.jpg',
-					 },
-					 {name:'浪子回头',
-					  author:'辉气*蛋',
-					  src:'http://cdnringbd.shoujiduoduo.com/ringres/userv1/a48/645/66725645.aac',
-					  img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547631130547&di=076c9275ce160baa2ff5cb66eed4ad53&imgtype=0&src=http%3A%2F%2Fwww.mixtapetorrent.com%2Fsystem%2Ffiles%2Ffollowmernb.jpg',
-					  },
-					 {name:'生僻字心动不已',
-					  author:'〃°ω°〃',
-					  src:'http://cdnringbd.shoujiduoduo.com/ringres/userv1/a48/359/64750359.aac',
-					  img:'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1547631330468&di=517d6b42ed32567a5798fd9bbe13ff9c&imgtype=0&src=http%3A%2F%2F5b0988e595225.cdn.sohucs.com%2Fimages%2F20170830%2F2dbe36dfa0a14c0c94b672f445c02a21.jpeg',
-					  }
 				],
 				audioPlaySrc:0,//当前播放的歌曲index
 				audioWay:0,//播放方式 0顺序播放 1随机播放 2单曲循环
@@ -104,6 +74,7 @@
 			}
 		},
 		onLoad(option) {
+			 console.log(option)
 			uni.request({
 				url: 'http://lqwan.club:3000/song/url?id=', //热歌榜
 				data: {
@@ -111,7 +82,10 @@
 				},
 				method: "GET",
 				success: (res) => {
-					console.log(res.data.data[0].url)
+					if(res.data.data[0].url==null){
+						res.data.data[0].url="https://music.163.com/song/media/outer/url?id="+option.id+".mp3"
+					}
+					this.audioList=[];
 					audio.push({
 						name:option.name,
 						author:"待修复",
@@ -119,6 +93,10 @@
 						src:res.data.data[0].url
 					});
 					this.audioList=audio;
+					console.log(res.data.data[0].url)
+					console.log(this.audioList)
+					this.audioPlaySrc = 0
+					this.audioInit()
 				}
 			});
 			console.log(option)
@@ -141,8 +119,8 @@
 			}
 		},
 		mounted:function() {
-			this.audioPlaySrc = 0
-			this.audioInit()
+			// this.audioPlaySrc = 0
+			// this.audioInit()
 		},
 		methods:{
 			audioInit(){
